6731da0fecc3a166b3e7126aa04f4ee20b469d27
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-06 13:38:32 -0400

    fix prettier formatting errors, add morgan and method-override,

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
-
-app.use(express.urlencoded({ extended: false }));
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
 	console.log(`connected to mongodb ${mongoose.connection.name}`);
 });
 const Book = require('./models/book');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
 
 app.get('/', async (req, res) => {
 	const allBooks = await Book.find();
@@ -21,28 +25,20 @@ app.get('/books/new', (req, res) => {
 	res.render('books/new.ejs');
 });
 
-app.get(
-	'/books/:bookId',
-	async (
-		req,
-		res
-	) => {
-		const foundBook =
-			await Book.findById(
-				req
-					.params
-					.bookId
-			);
-		res.render(
-			'books/show.ejs',
-			{
+app.get('/books/:bookId', async (req, res) => {
+	const foundBook = await Book.findById(req.params.bookId);
+	res.render('books/show.ejs', {
 				book: foundBook,
-			}
-		);
-	}
-);
+	});
+});
+
+app.get('/books/:bookId/edit', async (req, res) => {
+	const foundBook = await Book.findById(req.params.bookId);
+	res.render('books/edit.ejs', { book: foundBook });
+});
 
 app.post('/books', async (req, res) => {
+	console.log(req.body.publicationYear);
 	await Book.create(req.body);
 	res.redirect('/');
 });
